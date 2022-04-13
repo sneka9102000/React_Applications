adba31ec7c5905dc8431bb783fe9d9d50eb7ce57
sneka9102000 <96578887+sneka9102000@users.noreply.github.com>
2022-04-13 10:01:55 +0530

    Add files via upload

---
+++ react-components/src/components/Greet.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+// function Greet () 
+// {
+//     return <h1>Hello Sneka</h1>
+// }
+const Greet  = () => <h1> Hello Sneka.Example for functional component</h1>
+export default Greet 
