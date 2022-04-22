64348b8ff458b5779128cd219b311112e596ccd2
sneka9102000 <96578887+sneka9102000@users.noreply.github.com>
2022-04-22 14:45:33 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import { Home } from './components/Home'
+import { About } from './components/About'
+import { Navbar } from './components/Navbar'
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='about' element={<About />}></Route>
+      </Routes>
+    </>
+
+      );
+}
+
+      export default App;
