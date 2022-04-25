dfcaf45242198645d6204d0da00b6a530257e5c7
sneka9102000 <96578887+sneka9102000@users.noreply.github.com>
2022-04-25 09:07:09 +0530

    Delete src directory

--- src/components/Navbar.js
+++
@@ -1,9 +0,0 @@
-import { Link } from 'react-router-dom'
-export const Navbar = () => {
-    return  (
-        <nav>
-            <Link to='/'>Home</Link>
-            <Link to='/about'>About</Link>
-        </nav>
-    )
-}
